bf33510796b12a5e59605999bef83183595bb4bf
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-08 21:26:27 -0500

    signupform

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -11,6 +12,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -21,26 +23,37 @@ const SignupForm = () => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+//    const form = event.currentTarget;
+//   if (form.checkValidity() === false) {
+//      event.preventDefault();
+//      event.stopPropagation();
+//    }
 
-    try {
-      const response = await createUser(userFormData);
+try {
+  const { data } = await addUser({
+    variables: { ...userFormData }
+  });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+  Auth.login(data.addUser.token);
+} catch (e) {
+  console.error(e);
+  setShowAlert(true);
+};
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+//    try {
+//      const response = await createUser(userFormData);
+
+//      if (!response.ok) {
+//        throw new Error('something went wrong!');
+//      }
+
+//      const { token, user } = await response.json();
+//      console.log(user);
+//      Auth.login(token);
+//    } catch (err) {
+//      console.error(err);
+//      setShowAlert(true);
+//    }
 
     setUserFormData({
       username: '',
