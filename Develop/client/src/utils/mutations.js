75f1069d0dba1d0a1a3b7536bac7a2cde22ed444
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-08 20:56:33 -0500

    line 15-26

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -5,10 +5,22 @@ export const LOGIN_USER = gql `
         login(email: $email, password: $password){
             token
             user {
-                _id
-                username
+            _id
+            username
 }
 }
 }
 `;
 
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+            _id
+            username
+            email
+}
+}
+}
+`;
