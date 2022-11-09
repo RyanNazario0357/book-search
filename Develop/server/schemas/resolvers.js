f32e19312ecb245d0812d719dc8f7c264fcc858a
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-08 21:37:41 -0500

    lines 11-23

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -8,3 +8,16 @@ const resolvers = {
         if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
         .select('-__v -password')
+
+    return userData;
+}
+    throw new AuthenticationError('Not logged in!');
+}
+},
+Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+
+            return { token, user};
+},
